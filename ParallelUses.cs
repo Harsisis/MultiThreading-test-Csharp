b8820d0a5538e8d5e2cb0707204995681272b333
Haorsis <cadetgauthier3@gmail.com>
2024-01-17 10:27:10 +0100

    step 1 to 5 exercice 1

---
+++ ParallelUses.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTest {
+    public static class ParallelUses {
+        public static async void DisplayListNumbersUsingParallelForeach(List<int> list, int sleepTimer) {
+            Console.WriteLine("{0} Entering in method DisplayListNumbersUsingParallelForeach", DateTime.Now);
+            Parallel.ForEach(list, number => {
+                Console.WriteLine("Current number is {0}", number);
+                Thread.Sleep(sleepTimer);
+            });
+            Console.WriteLine("{0} Out of method DisplayListNumbersUsingParallelForeach", DateTime.Now);
+        }
+
+        public static async void DisplayListNumbersUsingParallelFor(List<int> list, int sleepTimer) {
+            Console.WriteLine("{0} Entering in method DisplayListNumbersUsingParallelFor", DateTime.Now);
+            Parallel.For(0, list.Count,
+                   number => {
+                       Console.WriteLine("Current number is {0}", list[number]);
+                       Thread.Sleep(sleepTimer);
+                   });
+            Console.WriteLine("{0} Out of method DisplayListNumbersUsingParallelFor", DateTime.Now);
+        }
+
+        public static async void DisplayListNumbersUsingParallelInvoke(List<int> list, List<int> list2) {
+            Console.WriteLine("{0} Entering in method DisplayListNumbersUsingParallelInvoke", DateTime.Now);
+            Parallel.Invoke(() => {
+                DisplayListNumbersUsingParallelForeach(list, 1000);
+            },
+            () => {
+                DisplayListNumbersUsingParallelForeach(list2, 1500);
+            });
+            Console.WriteLine("{0} Out of method DisplayListNumbersUsingParallelInvoke", DateTime.Now);
+        }
+
+        public static int SumListNumbersUsingParallelInvokeAndAggregate(List<int> list) {
+            Console.WriteLine("{0} Entering in method SumListNumbersUsingParallelInvokeAndAggregate", DateTime.Now);
+            var sum = 0;
+            Parallel.Invoke(() => {
+                sum = list.Aggregate((a, b) => a + b);
+            });
+            Console.WriteLine("{0} Out of method SumListNumbersUsingParallelInvokeAndAggregate", DateTime.Now);
+            return sum;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Threading.Tasks;
 using TaskTest;
 
 var timer = new Stopwatch();
@@ -29,8 +30,36 @@ timer.Stop();
 Console.WriteLine("{0} DelayedTasks", timer.Elapsed.ToString());*/
 
 
-timer.Reset();
+/*timer.Reset();
 timer.Start();
 WaysToUseAsync.ParallelHeavyComputation();
 timer.Stop();
-Console.WriteLine("{0} ParallelHeavyComputation", timer.Elapsed.ToString());
+Console.WriteLine("{0} ParallelHeavyComputation", timer.Elapsed.ToString());*/
+
+
+List<int> list = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+/*
+timer.Reset();
+timer.Start();
+ParallelUses.DisplayListNumbersUsingParallelForeach(list, 1000);
+timer.Stop();
+Console.WriteLine("{0} DisplayListNumbersUsingParallelForeach", timer.Elapsed.ToString());
+
+timer.Reset();
+timer.Start();
+ParallelUses.DisplayListNumbersUsingParallelFor(list, 1000);
+timer.Stop();
+Console.WriteLine("{0} DisplayListNumbersUsingParallelFor", timer.Elapsed.ToString());*/
+
+List<int> list2 = new List<int>() { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+/*timer.Reset();
+timer.Start();
+ParallelUses.DisplayListNumbersUsingParallelInvoke(list, list2);
+timer.Stop();
+Console.WriteLine("{0} DisplayListNumbersUsingParallelInvoke", timer.Elapsed.ToString());*/
+
+List<int> numbers = new List<int>();
+for (int i = 1; i <= 100; i++) {
+    numbers.Add(i);
+}
+Console.WriteLine("La somme des valeurs de 1 à 100 est : {0}", ParallelUses.SumListNumbersUsingParallelInvokeAndAggregate(numbers));
